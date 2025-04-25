4635b624b66914212b9b9f065c4176df6f84ccb1
Sai Kumar <167757736+SaiKumar7465@users.noreply.github.com>
2025-04-25 15:34:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMessage() {
+    const msg = document.getElementById('message');
+    msg.textContent = msg.textContent === "Welcome to my page" ? "Hello!" : "Welcome to my page";
+  }
+  
